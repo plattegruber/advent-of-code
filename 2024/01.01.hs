e4e301c46704b2d3c375722338bc1b0007badf4b
plattegruber <gruberplatte@gmail.com>
2024-12-01 22:46:49 +0000

    solve 01.01

---
+++ 2024/01.01.hs
@@ -0,0 +1,11 @@
+import Data.List
+
+
+main :: IO ()
+main = do
+    input <- readFile "01.01.txt"
+    let (listOne, listTwo) = unzip $ map ((\[x, y] -> (read x, read y)) . words) (lines input)
+        sortedOne = sort listOne
+        sortedTwo = sort listTwo
+        totalDistance = sum $ zipWith (\x y -> abs(x - y)) sortedOne sortedTwo
+    print totalDistance
